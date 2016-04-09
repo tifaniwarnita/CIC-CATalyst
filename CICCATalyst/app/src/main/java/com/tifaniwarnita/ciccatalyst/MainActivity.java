38bca20bef4226916c860a9674bc4389ede01507
Tifani Warnita <tiffayumuyuka@gmail.com>
2016-04-09 22:12:59 +0700

    Add our menu fragment and fix backstack for nav drawer

--- CICCATalyst/app/src/main/java/com/tifaniwarnita/ciccatalyst/MainActivity.java
+++ CICCATalyst/app/src/main/java/com/tifaniwarnita/ciccatalyst/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,6 +18,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
+    static final int REQUEST_TOKEN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,7 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         // TODO: Tulisan Hi! di nav drawer diganti sama Hi, <<NAMA>>! kalau udah login
@@ -49,7 +53,35 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            int count = getSupportFragmentManager().getBackStackEntryCount();
+
+            if (count == 0) {
+                super.onBackPressed();
+                navigationView.getMenu().getItem(0).setChecked(true);
+                //additional code
+            } else {
+                getSupportFragmentManager().popBackStack();
+                if (count == 1) {
+                    navigationView.getMenu().getItem(0).setChecked(true);
+                } else {
+                    FragmentManager.BackStackEntry backEntry =
+                            getSupportFragmentManager().getBackStackEntryAt(
+                                    getSupportFragmentManager().getBackStackEntryCount()-2);
+                    String tag = backEntry.getName();
+                    Log.d("TAG BACKSTACK: ", tag);
+                    if (tag.equals(getResources().getString(R.string.event))) {
+                        navigationView.getMenu().getItem(2).setChecked(true);
+                    } else if (tag.equals(getResources().getString(R.string.our_cats))) {
+                        navigationView.getMenu().getItem(3).setChecked(true);
+                    } else if (tag.equals(getResources().getString(R.string.our_menu))) {
+                        navigationView.getMenu().getItem(4).setChecked(true);
+                    } else if (tag.equals(getResources().getString(R.string.reservation))) {
+                        navigationView.getMenu().getItem(5).setChecked(true);
+                    } else if (tag.equals(getResources().getString(R.string.contacts))) {
+                        navigationView.getMenu().getItem(6).setChecked(true);
+                    }
+                }
+            }
         }
     }
 
@@ -94,17 +126,21 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_login) {
             Intent intent = new Intent(MainActivity.this, AuthActivity.class);
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-            startActivity(intent);
+            startActivityForResult(intent, REQUEST_TOKEN);
         } else if (id == R.id.nav_event) {
             FragmentManager fm = getSupportFragmentManager();
             fm.beginTransaction()
                     .replace(R.id.fragment_container, new EventFragment())
-                    .addToBackStack(null)
+                    .addToBackStack(getResources().getString(R.string.event))
                     .commit();
         } else if (id == R.id.nav_our_cats) {
 
         } else if (id == R.id.nav_our_menu) {
-
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction()
+                    .replace(R.id.fragment_container, new OurMenuFragment())
+                    .addToBackStack(getResources().getString(R.string.our_menu))
+                    .commit();
         } else if (id == R.id.nav_reservation) {
 
         } else if (id == R.id.nav_contacts) {
@@ -114,5 +150,20 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TOKEN) {
+            // Make sure the request was successful
+            navigationView.getMenu().getItem(0).setChecked(true);
+
+            if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                // Do something with the contact here (bigger example below)
+            }
+        }
     }
 }
---
+++ CICCATalyst/app/src/main/java/com/tifaniwarnita/ciccatalyst/OurMenuFragment.java
@@ -0,0 +1,28 @@
+package com.tifaniwarnita.ciccatalyst;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class OurMenuFragment extends Fragment {
+
+
+    public OurMenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_our_menu, container, false);
+        return v;
+    }
+
+
+}
